ea8118cbe0efab6eedd2f06918d7b4c1f3b8180d
theorclord <mstolbor@gmail.com>
2021-05-31 14:40:59 +0200

    Implemented simple ad

---
+++ Assets/Scripts/UnityObjects/AddPlayer.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AddPlayer : MonoBehaviour
+{
+    private bool testMode = true;
+    private bool addRun = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(PersistentData.Instance.AndroidGameId, testMode);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!addRun)
+        {
+            ShowInterstitialAd();
+        }
+    }
+
+    public void ShowInterstitialAd()
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady())
+        {
+            addRun = true;
+
+            // Id in parenteses is found on the unity dashboard
+            Advertisement.Show("Interstitial_Android");
+        }
+        else
+        {
+            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        }
+    }
+}
---
+++ Assets/Scripts/UnityObjects/PersistentData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PersistentData : MonoBehaviour
+    {
+        #region IngamgeVariables
+        private string _androidGameId;
+        public string AndroidGameId
+        {
+            get
+            {
+                if(_androidGameId == null)
+                {
+                    LoadSettingsJson();
+                }
+                return _androidGameId;
+            }
+            set
+            {
+                _androidGameId = value;
+            }
+        }
+        #endregion
+
+        #region PlayerPrefs
+        #endregion
+
+        public static PersistentData Instance { get; set; }
+        void Awake()
+        {
+            //If we don't currently have a game control...
+            if (Instance == null)
+            {
+                //...set this one to be it...
+                Instance = this;
+            }
+            //...otherwise...
+            else if (Instance != this)
+            {
+                //...destroy this one because it is a duplicate.
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(this);
+
+            // Load the soundval from last config file.
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            LoadSettingsJson();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void LoadSettingsJson()
+        {
+            using (StreamReader r = new StreamReader("secrets.json"))
+            {
+                string json = r.ReadToEnd();
+                Secret secrets = JsonUtility.FromJson<Secret>(json);
+                AndroidGameId = secrets.AndroidGameId;
+            }
+        }
+
+        public class Secret
+        {
+            public string AndroidGameId;
+        }
+    }
+}
